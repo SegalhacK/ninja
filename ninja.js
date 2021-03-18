ea5ec6a612c1f3ad24c1b05a59ad3266bddc48b5
Christian Manriquez <chrmanriquez@gmail.com>
2021-03-18 11:54:24 -0300

    [TAREA] Sensei

--- ninja.js
+++ ninja.js
@@ -1,17 +1,28 @@
 class Ninja {
-    constructor(name, hp, spd, str) {
+    constructor(name, hp = 10, spd = 3, str = 3) {
         this.name = name;
-        this.hp = 10;
-        this.spd = 3;
-        this.str = 3;
+        this.hp = hp;
+        this.spd = spd;
+        this.str = str;
     }
     sayName() {
         console.log(`Osu! Mi nombre es ${this.name}`);
     }
     showStats() {
-        console.log(`Nombre: ${this.name}, Velocidad: ${this.spd}, HP: ${this.hp}`);
+        console.log(`Nombre: ${this.name}\nVelocidad: ${this.spd}\nHP: ${this.hp}\n Fuerza: ${this.str}`);
     }
     drinkSake() {
         this.hp += 10;
     }
 };
+
+class Sensei extends Ninja {
+    constructor(name, hp = 10, spd = 3, str = 3, wisdom = 10) {
+        super(name, hp, spd, str)
+        this.wisdom = wisdom;
+    }
+    speakWisdom() {
+        super.drinkSake();
+        console.log('Tatakae!');
+    }
+}
